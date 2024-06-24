def81c912bace4684a16e710ab961b871def1579
Kira <kirillbutakov02021994@gmail.com>
2024-06-24 22:29:14 +0300

    fix_home_work

--- src/processing.py
+++ src/processing.py
@@ -1,68 +1,33 @@
 from typing import List, Dict
 
 
-# Первое задание.
-def filter_by_state(data: List[Dict[str, str]], state: str) -> List[Dict[str, str]]:
-    """
-    Фильтрует список словарей по состоянию.
-
-
-    data (List[Dict[str, str]]): Список словарей для фильтрации.
-    state (str): Состояние для фильтрации.
-
-    Returns:
-        List[Dict[str, str]]: Отфильтрованный список словарей.
-    """
-    return [entry for entry in data if entry.get("state") == state]
-
-
-def sort_key(entry: Dict[str, str]) -> str:
-    """
-    Функция для извлечения ключа 'date' из словаря.
-    """
-    return entry["date"]
-
-
-def sort_by_date(data: List[Dict[str, str]]) -> List[Dict[str, str]]:
-    """
-    Сортирует список словарей по дате.
-
-
-    data (List[Dict[str, str]]): Список словарей, которые нужно отсортировать.
-
-    Returns:
-        List[Dict[str, str]]: Отсортированный список словарей по дате.
-    """
-    return sorted(data, key=sort_key)
-
-
-# Второе задание.
 def get_date(dictionary: Dict[str, str]) -> str:
     """
     Функция для получения значения ключа 'date' из словаря.
-
     """
     return dictionary["date"]
 
 
 def sort_by_date(
-    data: List[Dict[str, str]], reverse: bool = False
+    data: List[Dict[str, str]], reverse: bool = True
 ) -> List[Dict[str, str]]:
     """
     Сортирует список словарей по убыванию даты.
 
-     data: список словарей
-    reverse: определяет порядок сортировки (по умолчанию False - по убыванию)
+    data: список словарей
+    reverse: определяет порядок сортировки (по умолчанию True - по убыванию)
     return: отсортированный список словарей
     """
     return sorted(data, key=get_date, reverse=reverse)
 
 
-def filter_by_state(data: List[Dict[str, str]], state: str) -> List[Dict[str, str]]:
+def filter_by_state(
+    data: List[Dict[str, str]], state: str = "EXECUTED"
+) -> List[Dict[str, str]]:
     """
     Фильтрует список словарей по ключу 'state'.
 
     data: список словарей
-    state: значение ключа 'state' для фильтрации
+    state: значение ключа 'state' для фильтрации (по умолчанию "EXECUTED")
     """
     return [item for item in data if item.get("state") == state]
